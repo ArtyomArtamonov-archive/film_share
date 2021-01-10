47ee2508425d3d14b09201ed199d28397c097636
Artyom <artem02artamonov@gmail.com>
2021-01-10 05:16:32 +0300

    streaming videos

--- FilmShare/Controllers/StreamController.cs
+++ FilmShare/Controllers/StreamController.cs
@@ -1,13 +1,21 @@
 using System.Diagnostics;
+using System.IO;
 using System.Threading.Tasks;
 using FilmShare.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FilmShare.Controllers{
     public class StreamController : Controller{
-        public async Task<FileStreamResult> FilmStream(){
-            var stream = System.IO.File.OpenRead("wwwroot/files/film.mp4");
-            return new FileStreamResult(stream, "video/mp4");
+        public async Task<IActionResult> FilmStream(){
+            string path = @"wwwroot/files/film.mp4";
+            var memory = new MemoryStream();
+            
+            using (var stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite, 65536, FileOptions.Asynchronous | FileOptions.SequentialScan))
+            {
+                await stream.CopyToAsync(memory);
+            }
+            memory.Position = 0;
+            return File(memory, "application/octet-stream", Path.GetFileName(path),true); 
         }
         
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
