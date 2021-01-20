1000d47f14542f4b7bd68257c86c727633cfb060
Artyom <artem02artamonov@gmail.com>
2021-01-20 08:58:09 +0300

    syncing is not working play/pause is sending

--- FilmShare/Classes/Hubs/FilmHub.cs
+++ FilmShare/Classes/Hubs/FilmHub.cs
@@ -6,14 +6,17 @@ namespace FilmShare.Hubs{
     public class FilmHub : Hub{
         public async Task SendTiming(string timing){
             await Clients.All.SendAsync("ReceiveFilmTiming", timing);
+            Console.WriteLine("Timimng");
         }
 
         public async Task SendStart(){
             await Clients.All.SendAsync("ReceiveStart");
+            Console.WriteLine("Start");
         }
         
         public async Task SendPause(){
             await Clients.All.SendAsync("ReceivePause");
+            Console.WriteLine("Pause");
         }
     }
 }
